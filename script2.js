3093e887314d321e101949c67c653f4e93000cb8
Kylychev <janat.kylychev.kk@gmail.com>
2022-01-28 14:31:48 +0400

    Done

---
+++ script2.js
@@ -0,0 +1,5 @@
+//2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение
+let message = "Maksat will learn JS";
+document.write(message+"<br>");
+message ="Jannat also will learn JS"
+document.write(message+"<br>");
