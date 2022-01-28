acf3747cdfea7f3e31519d9a7850d999b4f90ade
Kylychev <janat.kylychev.kk@gmail.com>
2022-01-29 00:28:05 +0400

    fix: add change js1

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 //1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере
-let showMessage = "Hello Janat!!!";
+let showMessage = "Hello Janat!";
 document.write(showMessage+"<br>");
 
 
